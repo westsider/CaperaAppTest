082c0e65ae866e3e9698b6fed06e9471de03756d
westsider <whansen1@mac.com>
2021-08-01 10:59:14 -0500

    removed switch

--- _AR Tutorials/11-facial-blend-shapes/projects/starter/ARFunnyFace/ContentView.swift
+++ _AR Tutorials/11-facial-blend-shapes/projects/starter/ARFunnyFace/ContentView.swift
@@ -102,35 +102,12 @@ struct ARViewContainer: UIViewRepresentable {
             
             let arConfiguration = ARFaceTrackingConfiguration()
             uiView.session.run(arConfiguration, options:[.resetTracking, .removeExistingAnchors])
-            
-            switch(propId) {
-            case 0: // Eyes
-                let arAnchor = try! Experience.loadLazer()
-                uiView.scene.anchors.append(arAnchor)
-                lazer = arAnchor
-                //print(lazer.circleL?.anchor?.components)
-                makeText(textAnchor: arAnchor, message: "Loading")
-                break
-            case 1: // Glasses
-                let arAnchor = try! Experience.loadGlasses()
-                uiView.scene.anchors.append(arAnchor)
-                break
-            case 2: // Mustache
-                let arAnchor = try! Experience.loadMustache()
-                uiView.scene.anchors.append(arAnchor)
-                break
-            case 3: // Robot
-                let arAnchor = try! Experience.loadRobot()
-                uiView.scene.anchors.append(arAnchor)
-                robot = arAnchor
-                break
-            case 4:
-                let arAnchor = try! Experience.loadEyes()
-                uiView.scene.anchors.append(arAnchor)
-                break
-            default:
-                break
-            }
+       
+            let arAnchor = try! Experience.loadLazer()
+            uiView.scene.anchors.append(arAnchor)
+            lazer = arAnchor
+            makeText(textAnchor: arAnchor, message: "Loading")
+   
         }
     
     func makeText(textAnchor: Experience.Lazer, message: String) {
@@ -189,8 +166,7 @@ struct ARViewContainer: UIViewRepresentable {
             let yaw = -rotation.y*3
             let pitch = -rotation.x*3
             let roll = rotation.z*1.5
-            
-            
+    
             let yawS = String(format: "%.2f", -rotation.y*3 )
             let pitchS = String(format: "%.2f", -rotation.x*3  )
             let rollS =  String(format: "%.2f", rotation.z*1.5 )
@@ -198,10 +174,7 @@ struct ARViewContainer: UIViewRepresentable {
             var pan = false
             var tilt = false
             var rolly = false
-            
-            
-            
-            
+
             if pitch > -0.03 && pitch < 0.03 {
                 tilt = true
             } else { tilt = false }
@@ -219,133 +192,9 @@ struct ARViewContainer: UIViewRepresentable {
             } else {
                 print("ADJUST: \(pan ? "" : "pan \(yawS)") \(tilt ? "" : "tilt") \(rolly ? "" : "roll")")
             }
-            
-            //print("\(pan ? "pan" : "" ) \(tilt ? "tilt" : "" ) \(rolly ? "roll" : "" ) \(pitchS)")
-//            else {
-//                print("yaw: \(yawS), pitch: \(pitchS) roll: \(rollS)")
-//            }
-
-            //print("yaw: \(yawS), pitch: \(pitchS) roll: \(rollS)")
-            
-            //MARK: - TODO - make the color change to green when OK
-            //MARK: - TODO - add roll
-            
-            
-            //guard let comp = lazer.circleL?.anchor?.components else { return }
-            
-            // print("we have a lazer  comp: \(comp)\n")
-     
-            //print("\(lazer.circleL?.anchor) ::  \(lazer.laserL?.anchor )")
- 
-//            guard robot != nil else { return }
-
-//            var faceAnchor: ARFaceAnchor?
-//            for anchor in anchors {
-//                if let a = anchor as? ARFaceAnchor {
-//                    faceAnchor = a
-//                }
-//            }
-
-//          let blendShapes = faceAnchor?.blendShapes
-//            let eyeBlinkLeft = blendShapes?[.eyeBlinkLeft]?.floatValue
-//            let eyeBlinkRight = blendShapes?[.eyeBlinkRight]?.floatValue
-//            let browInnerUp = blendShapes?[.browInnerUp]?.floatValue
-//            let browLeft = blendShapes?[.browDownLeft]?.floatValue
-//            let browRight = blendShapes?[.browDownRight]?.floatValue
-//            let jawOpen = blendShapes?[.jawOpen]?.floatValue
-
-//            let blendShapes = lazer.circleL?.anchor?.convert(position: lazer.position, from: lazer)
-//
-//            let blendShapes2 = lazer.position(relativeTo: lazer.circleL)
-//
-//            let blendShapes3 = lazer.circleL?.position(relativeTo: lazer.laserL)
-            
-            
-            
-            
-            
-            
-//            var startPoint: SCNVector3!
-//            var endPoint: SCNVector3!
-//
-//            let cirL = lazer.circleL?.anchor?.transform
-//            print(cirL)
-//            guard let Circle_Lx = cirL?.x else { return }
-//            guard let Circle_Ly = cirL?.y else { return }
-//            guard let Circle_Lz = cirL?.z else { return }
-//
-//
-//
-//            let lazL = lazer.laserL?.transform
-//            guard let Laser_Lx = lazL?.x else { return }
-//            guard let Laser_Ly = lazL?.y else { return }
-//            guard let Laser_Lz = lazL?.z else { return }
-//
-//
-//            endPoint = SCNVector3(Circle_Lx, Circle_Ly, Circle_Lz)
-//            startPoint = SCNVector3(Laser_Lx, Laser_Ly, Laser_Lz)
-//
-//
-//
-//            var dist = HHelper.distanceFrom(vector: startPoint, toVector: endPoint)
-//            let distS = String(format: "%.6f", dist * 1000)
-//            print(distS)
-            
-            
-//            let Circle_Lxs = String(format: "%.2f", Circle_Lx * 100.0)
-//            let Circle_Lys = String(format: "%.2f", Circle_Ly * 100.0)
-//           // print("Circle_L: x,y \t\t\t\(Circle_Lxs), \t\(Circle_Lys)")
-//
-//            guard let Laser_Rx = cirL?.columns.1.x else { return }
-//            guard let Laser_Ry = cirL?.columns.1.y else { return }
-//            let Laser_Rxs = String(format: "%.2f", Laser_Rx * 100.0)
-//            let Laser_Rys = String(format: "%.2f", Laser_Ry * 100.0)
-//            //print("Laser_R: x,y \t\t\t\(Laser_Rxs), \t\(Laser_Rys)")
-//
-//            guard let Circle_Rx = cirL?.columns.2.x else { return }
-//            guard let Circle_Ry = cirL?.columns.2.y else { return }
-//            let Circle_Rxs = String(format: "%.2f", Circle_Rx * 100.0)
-//            let Circle_Rys = String(format: "%.2f", Circle_Ry * 100.0)
-//            //print("Circle_Rx: x,y \t\t\t\(Circle_Rxs), \t\(Circle_Rys)")
-//
-//            guard let Laser_Lx = cirL?.columns.3.x else { return }
-//            guard let Laser_Ly = cirL?.columns.3.y else { return }
-//            let Laser_Lxs = String(format: "%.2f", Laser_Lx * 100.0)
-//            let Laser_Lys = String(format: "%.2f", Laser_Ly * 100.0)
-            //print("Laser_L: x,y \t\t\t\(Laser_Lxs), \t\(Laser_Lys)")
-            
-            
             func Deg2Rad(_ value: Float) -> Float {
               return value * .pi / 180
             }
-            
-    
-       
-            
-            
-            
-//
-//            robot.eyeLidL?.orientation = simd_mul(
-//
-//              simd_quatf(
-//                angle: Deg2Rad(-120 + (90 * eyeBlinkLeft!)),
-//                axis: [1, 0, 0]),
-//
-//              simd_quatf(
-//                angle: Deg2Rad((90 * browLeft!) - (30 * browInnerUp!)),
-//                axis: [0, 0, 1]))
-//
-//            robot.eyeLidR?.orientation = simd_mul(
-//              simd_quatf(
-//                angle: Deg2Rad(-120 + (90 * eyeBlinkRight!)),
-//                axis: [1, 0, 0]),
-//              simd_quatf(
-//                angle: Deg2Rad((-90 * browRight!) - (-30 * browInnerUp!)),
-//                axis: [0, 0, 1]))
-//
-//            robot.jaw?.orientation = simd_quatf(
-//              angle: Deg2Rad(-100 + (60 * jawOpen!)),
-//              axis: [1, 0, 0])
         }
     }
 }
@@ -369,18 +218,6 @@ extension SCNGeometry {
     }
 }
 
-//extension SCNVector3 {
-//    static func distanceFrom(vector vector1: SCNVector3, toVector vector2: SCNVector3) -> Float {
-//        let x0 = vector1.x
-//        let x1 = vector2.x
-//        let y0 = vector1.y
-//        let y1 = vector2.y
-//        let z0 = vector1.z
-//        let z1 = vector2.z
-//
-//        return sqrtf(powf(x1-x0, 2) + powf(y1-y0, 2) + powf(z1-z0, 2))
-//    }
-//}
 
 class HHelper {
     static func distanceFrom(vector vector1: SCNVector3, toVector vector2: SCNVector3) -> Float {
